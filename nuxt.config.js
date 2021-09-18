7b7478451329ec0a66d8ac7dda5ef148c29586bd
Iván Martínez <ivanmartinez33@outlook.com>
2021-09-17 19:44:28 -0500

    Se actualizó el favicon y el nombre de la página

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,9 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'ICI',
+    title: 'ici | Clínica dental',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'es'
     },
     meta: [
       { charset: 'utf-8' },
