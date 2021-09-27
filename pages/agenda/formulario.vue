be7177e882c2135e0c34e919e78c7a10df2fabc1
Iván Martínez <ivanmartinez33@outlook.com>
2021-09-27 18:16:08 -0500

    Ya se guardan los datos del usuario en el estado

--- pages/agenda/formulario.vue
+++ pages/agenda/formulario.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-white pt-12">
     <!-- Formulario -->
-    <form action="" class="form max-w-5xl mx-auto p-6 my-10 relative">
+    <form @submit="submitForm" class="form max-w-5xl mx-auto p-6 my-10 relative">
       <h3 class="text-2xl text-gray-900 font-semibold">
         Ya casi tienes tu cita
       </h3>
@@ -13,25 +13,46 @@
         <!-- Nombre -->
         <div class="w-full md:w-1/2">
           <label for="first_name" class="block text-xs font-semibold text-gray-600 uppercase mb-2 required">NOMBRE(S)</label>
-          <input type="text" name="first_name" id="first_name" placeholder="Tu nombre" class="border p-2 w-full" required>
+          <input v-model="patient.firstName" type="text" name="first_name" id="first_name" placeholder="Tu nombre" class="border p-2 w-full" required>
         </div>
         <!-- Apellidos -->
         <div class="w-full md:w-1/2">
           <label for="last_name" class="block text-xs font-semibold text-gray-600 uppercase mb-2 mt-8 md:mt-0 required">APELLIDOS</label>
-          <input type="text" name="last_name" id="last_name" placeholder="Tus apellidos" class="border p-2 w-full" required>
+          <input v-model="patient.lastName" type="text" name="last_name" id="last_name" placeholder="Tus apellidos" class="border p-2 w-full" required>
         </div>
       </div>
       <!-- Número de teléfono -->
       <label for="phone_number" class="block text-xs font-semibold text-gray-600 uppercase mb-2 mt-8 required">Número de teléfono</label>
-      <input type="tel" name="phone_number" id="phone_number" placeholder="Tu número de teléfono" class="border p-2 w-full" required>
+      <input v-model="patient.phoneNumber" type="tel" name="phone_number" id="phone_number" placeholder="Tu número de teléfono" class="border p-2 w-full" required>
       <!-- Correo electrónico -->
       <label for="email" class="block text-xs font-semibold text-gray-600 uppercase mb-2 mt-8">Correo electrónico</label>
-      <input type="email" name="email" id="email" placeholder="Tu correo electrónico" class="border p-2 w-full mt-3 mb-4">
+      <input v-model="patient.email" type="email" name="email" id="email" placeholder="Tu correo electrónico" class="border p-2 w-full mt-3 mb-4">
       <!-- Botón para enviar el cuestionario -->
       <input type="submit" value="Continuar" class="w-full mt-6 bg-pink-500 active:bg-pink-600 text-white font-semibold p-3 rounded-lg cursor-pointer">
   </form>
   </div>
 </template>
+
+<script>
+// Importar dependencias
+import { mapMutations } from 'vuex'
+
+export default {
+  data: () => ({
+    patient: {
+      firstName: '',
+      lastName: '',
+      phoneNumber: '',
+      email: ''
+    }
+  }),
+  methods: {
+    submitForm() {
+      this.$store.commit('form/setFormData', this.patient)
+    }
+  }
+}
+</script>
 
 <style scoped>
 .required:after {
---
+++ store/form.js
@@ -0,0 +1,9 @@
+export const state = () => ({
+  formData: null
+})
+
+export const mutations = {
+  setFormData(state, formData) {
+    state.formData = formData
+  }
+}
