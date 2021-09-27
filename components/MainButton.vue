ef46ee9fe2480be1f5ece601ecd8def7b4e65892
Iván Martínez <ivanmartinez33@outlook.com>
2021-09-27 17:44:46 -0500

    Prueba del CTA

--- components/MainButton.vue
+++ components/MainButton.vue
@@ -1,11 +1,15 @@
 <template>
-  <button class="bg-pink-500 active:bg-pink-600 text-white px-8 py-4 rounded-lg font-medium tracking-widest">
+  <button class="bg-pink-500 active:bg-pink-600 text-white px-8 py-4 rounded-lg font-medium tracking-widest" @click="onClick">
     <slot class="font-medium"></slot>
   </button>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    onClick() {
+      this.$emit('click')
+    }
+  }
 }
 </script>
