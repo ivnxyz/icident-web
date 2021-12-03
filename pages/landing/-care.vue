1b6999adf699ce758f2f656b01c798a6987f96ae
Iván Martínez <ivanmartinez33@outlook.com>
2021-12-03 17:02:09 -0600

    Se agregó la sección de qué esperar en la primera cita

--- pages/landing/-care.vue
+++ pages/landing/-care.vue
@@ -1,58 +1,140 @@
 <template>
-  <section class="pt-20 my-8 max-w-5xl mx-auto">
-    <article class="bg-white border-2 border-gray-200 rounded-lg p-8 mx-6">
-      <h3 class="font-bold">
-        ¿Qué debes esperar en tu primera visita?
-      </h3>
-      Ir al dentista puede provocar ansiedad, pero aquí te decimos todo lo que debes saber para hacer tu primera al dentista lo más confortable y libre de estrés posible.
-
-      <h3 class="font-bold mt-8">
-        Registro
-      </h3>
-
-      Al llegar a clínica inicialmente es necesario cumplir con el protocolo de sanidad por el COVID 19, se tomará tu temperatura y se te pedirá aplicar gel antibacterial en manos, posteriormente se te pedirán algunos datos de registro que formarán parte de tu historia clínica.
-
-      <h3 class="font-bold mt-8">
-        Diagnóstico Médico
-      </h3>
-
-      Nuestro odontólogo general continuará con algunos cuestionamientos sobre tu situación de salud, alergias e intervenciones quirúrgicas previas, de esta manera tu historia clínica estará completa. Posteriormente, hará una revisión visual en tu boca y evaluará si el necesario tomar alguna placa de rayos X para tener un diagnóstico preciso.
-      Una vez realizado el diagnóstico se te informará a detalle el resultado y lo más importante se te explicará a detalle el plan de tratamiento a seguir para que recuperes al 100% tu salud bucal.
-
-      <h3 class="font-bold mt-8">
-        Formas de pago
-      </h3>
-
-      <ul>
-        <li class="text-green-600">
-          <outline-check-circle-icon class="w-6 h-6 inline" /> Efectivo
-        </li>
-        <li class="text-green-600">
-          <outline-check-circle-icon class="w-6 h-6 inline" /> Transferencias
-        </li>
-        <li class="text-green-600">
-          <outline-check-circle-icon class="w-6 h-6 inline" /> Tarjeta de crédito o débito
-        </li>
-        <li class="text-yellow-600">
-          <outline-exclamation-circle-icon class="w-6 h-6 inline" /> NO trabajamos con ninguna aseguradora
-        </li>
-      </ul>
-
-      <h3 class="font-bold mt-8">
-      Alternativas de financiamiento
-      </h3>
-
-      En ICI el paciente es nuestra prioridad, en el caso de un tratamiento largo y con un presupuesto elevado tenemos alternativas de financiamiento ya sea directamente con nosotros o a través de nuestro aliado Alivio Capital financiera especializada en otorgar préstamos para tratamiento de salud.
-    </article>
+  <section class="">
+    <!-- Título -->
+    <SectionDivider>
+      ¿QUÉ DEBES ESPERAR EN TU PRIMERA VISITA?
+    </SectionDivider>
+    <!-- Contenido -->
+    <div class="grid grid-rows-4 gap-8 md:gap-8 grid-cols-1 md:grid-rows-2 md:grid-cols-2 lg:grid-rows-1 lg:grid-cols-4 mx-8 md:mx-10 mt-8 md:mt-0">
+      <!-- Registro -->
+      <article class="grid grid-rows-2">
+        <div class="flex flex-col justify-center items-center">
+          <img class="object-contain object-center w-32 h-32" src="@/assets/icons/registro.svg" alt="">
+          <p class="text-center mt-3 text-lg font-bold text-gray-500">REGISTRO</p>
+        </div>
+        <div class="bg-gray-200 rounded-lg p-3 shadow-lg">
+          <p>
+            Al llegar a clínica inicialmente es necesario cumplir con el protocolo de sanidad con el COVID 19, se tomará tu temperatura y se te pedirá aplicar gel antibacterial en manos, posteriormente se te pedirán algunos datos de registro que formarán parte de tu historia clínica.
+          </p>
+        </div>
+      </article>
+      <!-- Diagnóstico -->
+      <article class="grid grid-rows-2">
+        <div class="flex flex-col justify-center items-center">
+          <img class="w-32 h-32" src="@/assets/icons/diagnostico.svg" alt="">
+          <p class="mt-2 text-lg font-bold text-gray-500">DIAGNÓSTICO</p>
+        </div>
+        <div class="bg-gray-200 rounded-lg p-3 shadow-lg">
+          <p>
+            Nuestro odontólogo general continuará con algunos cuestionamientos sobre tu situación de salud, alergias e intervenciones quirúrgicas previas, de esta manera tu historia clínica estará completa. Posteriormente, hará una revisión visual en tu boca y evaluará...
+          </p>
+          <button class="text-center w-full font-bold mt-3 mb-2 text-gray-400 underline" href="">
+            Ver más
+          </button>
+        </div>
+      </article>
+      <!-- Formas de pago -->
+      <article class="grid grid-rows-2">
+        <div class="flex flex-col justify-center items-center">
+          <img class="object-contain object-center w-32 h-32" src="@/assets/icons/formas-de-pago.svg" alt="">
+          <p class="mt-2 text-lg font-bold text-gray-500">FORMAS DE PAGO</p>
+        </div>
+        <div class="bg-gray-200 rounded-lg p-3 shadow-lg">
+          <div class="step">
+            <div>
+                <div class="circle">
+                  <img src="@/assets/icons/check.svg" alt="Icono de verificación">
+                </div>
+            </div>
+            <div>
+                <div class="title">Efectivo</div>
+            </div>
+          </div>
+          <div class="step step-active">
+            <div>
+                <div class="circle">
+                  <img src="@/assets/icons/check.svg" alt="Icono de verificación">
+                </div>
+            </div>
+            <div>
+                <div class="title">Transferencias</div>
+            </div>
+          </div>
+          <div class="step">
+            <div>
+                <div class="circle">
+                  <img src="@/assets/icons/check.svg" alt="Icono de verificación">
+                </div>
+            </div>
+            <div>
+                <div class="title">Tarjeta de crédito<br>o débito</div>
+            </div>
+          </div>
+        </div>
+      </article>
+      <!-- Financiamiento -->
+      <article class="grid grid-rows-2">
+        <div class="flex flex-col justify-center items-center">
+          <img class="object-contain object-center w-32 h-32" src="@/assets/icons/financiamiento.svg" alt="">
+          <p class="mt-2 text-lg font-bold text-gray-500">FINANCIAMIENTO</p>
+        </div>
+        <div class="bg-gray-200 rounded-lg p-3 shadow-lg">
+          <p>
+            En ICI el paciente es nuestra prioridad, en el caso de un tratamiento largo y con un presupuesto elevado tenemos alternativas de financiamiento ya sea directamente con nosotros o a través de nuestro aliado Alivio Capital financiera especializada en otorgar préstamos para tratamiento de salud.
+          </p>
+        </div>
+      </article>
+    </div>
   </section>
 </template>
 
-<script>
-export default {
-
+<style scoped>
+.step {
+  position: relative;
+  min-height: 1em;
 }
-</script>
+.title {
+  line-height: 1.5em;
+}
+.caption {
+  font-size: 0.8em;
+}
+.step + .step {
+  margin-top: 1.5em
+}
+.step > div:first-child {
+  position: static;
+  height: 0;
+}
+.step > div:not(:first-child) {
+  margin-left: 1.5em;
+  padding-left: 1em;
+}
 
-<style>
+.circle {
+  position: relative;
+  width: 1.5em;
+  height: 1.5em;
+  border-radius: 100%;
+  text-align: center;
+}
 
+.circle:after {
+  content: '';
+  position: absolute;
+  display: block;
+  top: -2px;
+  right: 50%;
+  bottom: 1px;
+  left: calc(50% - calc(3px/2));
+  height: 100%;
+  width: 3px;
+  transform: scale(1, 2);
+  transform-origin: 50% -100%;
+  background-color: rgba(0,0,0);
+}
+
+.step:last-child .circle:after {
+  display: none
+}
 </style>
