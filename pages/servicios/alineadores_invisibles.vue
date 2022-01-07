a2a1f8651315e14e19429f5a72e947bfff64dc51
Iván Martínez <ivanmartinez33@outlook.com>
2022-01-07 14:24:06 -0600

    Se agregó el servicio de alineadores invisibles

---
+++ pages/servicios/alineadores_invisibles.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="flex items-center justify-center max-w-5xl mx-2 lg:mx-auto">
+    <article class="text-gray-800">
+      <p>
+        El color blanco de tus dientes es lo primero que se parecía cuando sonríes, si no  quieres utilizar los tradicionales brackets metálicos o de zafiro y deseas corregir tu sonrisa los alineadores dentales pueden ser la mejor opción para ti.
+      </p>
+      <p class="mt-8">
+        Los alineadores dentales tiene muchas ventajas sobre los brackets tradicionles:  son más cómodos, más estéticos, el tratamiento se acorta, son más higiénicos, en resumen tienen una tecnología más avanzada.
+      </p>
+      <p class="mt-8">
+        Sin embargo, los alineadores no son para todos los pacientes por lo que deberás asistir a una cita diagnóstico SIN COSTO donde un especialista evaluará tu caso y te hará saber si eres candidato o no para alineadores invisibles.
+      </p>
+      <p class="mt-8">
+        Al ser una tecnología nueva indudablemente es más costosa que los Brackets tradicionales, sin embargo en ICI hemos hecho lo posible para que esta nueva tecnología sea más accesible, te sorprenderás de nuestros precios!! Agenda una cita de valoración SIN COSTO YA y entérate de todos los detalles!!
+      </p>
+    </article>
+  </section>
+</template>
