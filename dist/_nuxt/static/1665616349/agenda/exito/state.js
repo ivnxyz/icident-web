084756fa2afb0fc8e9fa991e0c23159e3535bda7
Iván Martínez <ivanmartinez33@outlook.com>
2022-10-12 18:20:16 -0500

    distW

--- dist/_nuxt/static/1665616349/agenda/exito/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1665616349",layout:"default",error:a,state:{form:{userData:a},schedule:{appointmentData:a}},serverRendered:true,routePath:"\u002Fagenda\u002Fexito",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
